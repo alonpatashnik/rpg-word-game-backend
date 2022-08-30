76890c97fa55808ca1a29afea3af1cc5a3c99d95
bunny <deamonbunny@gmail.com>
2022-08-30 14:02:54 -0700

    started filling out models

---
+++ models/Character.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Character extends Model {}
+
+Character.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    charName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            min: 3,
+            max: 15
+        }
+    },
+    charJpg: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+})
+
+module.exports = Character
