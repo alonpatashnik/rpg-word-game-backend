adde1364db896d815f5b4d446d558041122c3069
alonpatashnik <alonp714@gmail.com>
2022-08-31 10:40:01 -0700

    user routes added

--- models/User.js
+++ models/User.js
@@ -5,12 +5,6 @@ const bcrypt = require("bcrypt")
 class User extends Model {}
 
 User.init({
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-    },
     username: {
         type: DataTypes.STRING,
         allowNull: false,
--- models/index.js
+++ models/index.js
@@ -2,11 +2,8 @@ const User = require('./User')
 const Character = require('./Character')
 const Prompt = require('./Prompt')
 const Word = require('./Word');
-const sequelize = require('sequelize');
 
-User.hasOne(Character, {
-    foreignKey: 'id'
-});
+User.hasOne(Character);
 Character.belongsTo(User);
 
 module.exports = { User, Character, Prompt, Word };
